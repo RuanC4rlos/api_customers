33d7d414ff958af465a5805342a60f3c83d0d0be
Ruan C4rlos <ruancarlos@ufpi.edu.br>
2025-03-30 15:31:16 -0300

    config postgres database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+export default  {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "123456",
+  database: "secret",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+}
